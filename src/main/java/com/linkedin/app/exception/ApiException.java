fe50ea94a437010882e8ba4944136661141736e8
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-07-07 23:37:03 +0300

    Added H2 dependency to POM.xml; Added basic project structure

---
+++ src/main/java/com/linkedin/app/exception/ApiException.java
@@ -0,0 +1,17 @@
+package com.linkedin.app.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@Data
+@AllArgsConstructor
+@ToString
+public class ApiException {
+    private final String message;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime zonedDateTime;
+}
