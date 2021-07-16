83bf6dea643ecab12533cdb6b607829758f84b64
KostiantynRog <42935404+KostiantynRog@users.noreply.github.com>
2021-07-16 15:40:50 +0300

    post entity version 01 (#21)
    post entity was added

---
+++ src/main/java/com/danit/fs12/entity/Post.java
@@ -0,0 +1,46 @@
+package com.danit.fs12.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity(name = "Post")
+@Table(name = "posts")
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Post extends AbstractEntity{
+  private String title;
+  @Column(name = "main_text", length = 280)
+  private String mainText;
+  private String author;
+  private String comments;
+  private String likes;
+
+  @JsonIgnore
+  @ManyToOne(
+      cascade = {CascadeType.PERSIST})
+  @JoinTable(
+      name = "rel_user_posts",
+      joinColumns = {
+          @JoinColumn(
+              name = "post_id",
+              referencedColumnName = "id")
+      },
+      inverseJoinColumns = {
+          @JoinColumn(name = "user_id",
+              referencedColumnName = "id")
+      })
+  private User user;
+}
