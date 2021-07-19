cb4333acb9ebc5dc25f0d88f713356491f71178b
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-07-20 01:23:22 +0300

    Added shared components

---
+++ src/frontend/src/shared/CrossIcon/StyleCross.js
@@ -0,0 +1,11 @@
+import {makeStyles} from '@material-ui/core/styles'
+
+export default makeStyles(() => ({
+
+  cross: {
+    '& > .MuiSvgIcon-root': {
+      color: 'rgba(0,0,0,.5)',
+      fontSize: 35
+    }
+  }
+}))
---
+++ src/frontend/src/shared/LikeMiniIcon/StyleLikeMini.js
@@ -0,0 +1,11 @@
+import {makeStyles} from '@material-ui/core/styles'
+
+export default makeStyles(() => ({
+
+  likeMini: {
+    color: '#fff',
+    fontSize: 15,
+    backgroundColor: '#0a66c2',
+    borderRadius: 30
+  }
+}))
