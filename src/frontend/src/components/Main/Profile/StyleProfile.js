8bcda8f73222492356f64dfcd443087cc2b68404
Serhii <romaniuk87@gmail.com>
2021-07-21 02:26:34 +0300

    -- added application routes

---
+++ src/frontend/src/components/Main/Profile/StyleProfile.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+export default makeStyles(() => ({
+
+  text: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}))
