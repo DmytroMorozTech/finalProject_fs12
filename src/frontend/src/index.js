16c314a401956d35ffba2d229d444092f18c7d2a
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-07-21 23:52:24 +0300

    Added ThemeProvider to the project

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { ThemeProvider } from '@material-ui/styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <ThemeProvider theme={}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
