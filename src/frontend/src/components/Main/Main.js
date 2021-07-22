b87e47cdd48353da11b190e2bb480490d89210ae
Olegio <ogorski23@gmail.com>
2021-07-22 15:19:55 +0300

    Add some styles in .js

--- src/frontend/src/components/Main/Main.js
+++ src/frontend/src/components/Main/Main.js
@@ -1,1 +1,21 @@
 import React from 'react'
+import StyleMain from "./StyleMain";
+import {MainRight} from "./MainRight";
+import {MainLeft} from "./MainLeft";
+import {Feed} from "./Feed/Feed";
+
+function Main() {
+    const classes = StyleMain();
+
+    return (
+        <div classes={classes.container}>
+            <Section>
+                <MainLeft/>
+                <Feed/>
+                <MainRight/>
+            </Section>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/frontend/src/components/Main/MainRight.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {StyleMainRight} from "./StyleMainRight";
+
+function MainRight() {
+    const classes = StyleMainRight();
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.follow_card}>
+                <div className={classes.follow_card__title}>
+                    <h2>Add to your feed</h2>
+                </div>
+                <div className={classes.follow_card__news_list}>
+                    <li className={classes.news_list__li}>
+                        <a>
+                            <div className={classes.profile_pic}/>
+                        </a>
+                    </li>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainRight;
