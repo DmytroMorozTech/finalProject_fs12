460fbcabe8b4900329477da9f4393c0870d1e7fb
krozhkov09@gmail.com <krozhkov09@gmail.com>
2021-07-22 23:00:54 +0300

    in first commit sharebox was part of post, made it part of feed

--- src/frontend/src/components/Main/Feed/Feed.js
+++ src/frontend/src/components/Main/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import StyleNotifications from '../../Notifications/StyleNotifications'
 import Post from './Post/Post'
+import ShareBox from './ShareBox/ShareBox'
 
 function Feed () {
   const classes = StyleNotifications()
@@ -9,6 +10,7 @@ function Feed () {
       <h1 className={classes.text}>
       Welcome to the Home page!
       </h1>
+      <ShareBox/>
       <Post/>
     </div>
   )
--- src/frontend/src/components/Main/Feed/Post/Post.js
+++ src/frontend/src/components/Main/Feed/Post/Post.js
@@ -6,7 +6,6 @@ import Style from './styles'
 import { useState } from 'react'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import LikeMiniIcon from '../../../../shared/LikeMiniIcon/LikeMiniIcon'
-import ShareBox from './ShareBox/ShareBox'
 
 function Post ({quantityOfLikes = 10595, quantityOfComments = 420, quantityOfViews = 244688}) {
   const classes = Style()
@@ -15,7 +14,6 @@ function Post ({quantityOfLikes = 10595, quantityOfComments = 420, quantityOfVie
 
   return (
     <div>
-      <ShareBox/>
       <div className={classes.quantity}>
         <span>
           <LikeMiniIcon/>
