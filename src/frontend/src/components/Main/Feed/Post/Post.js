91c9598dba01d63e6ba523284069510bd11685b0
krozhkov09@gmail.com <krozhkov09@gmail.com>
2021-07-23 16:20:51 +0300

    part 1 of Post

--- src/frontend/src/components/Main/Feed/Post/Post.js
+++ src/frontend/src/components/Main/Feed/Post/Post.js
@@ -2,18 +2,42 @@ import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined'
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined'
 import RedoOutlinedIcon from '@material-ui/icons/RedoOutlined'
 import TelegramIcon from '@material-ui/icons/Telegram'
+import PublicIcon from '@material-ui/icons/Public'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import Style from './styles'
 import { useState } from 'react'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import LikeMiniIcon from '../../../../shared/LikeMiniIcon/LikeMiniIcon'
+import Avatar from '../../../../shared/Avatar/Avatar'
 
-function Post ({quantityOfLikes = 10595, quantityOfComments = 420, quantityOfViews = 244688}) {
+function Post ({userName = 'Steve Noiry', position = 'Java Developer', postTime = '1h', quantityOfLikes = 10595, quantityOfComments = 420, quantityOfViews = 244688}) {
   const classes = Style()
 
   const [liked, setLiked] = useState(false)
 
   return (
-    <div>
+    <div className={classes.post}>
+      <div className={classes.hiddenMenu}>
+        <MoreHorizIcon/>
+      </div>
+      <hr className={classes.line} />
+      <div className={classes.postAuthor}>
+        <Avatar/>
+        <div className={classes.userInfo}>
+          <div className={classes.name}>
+            {userName}
+          </div>
+          <div className={classes.position}>
+            {position}
+          </div>
+          <div className={classes.postTime}>
+            {postTime}
+            <div className={classes.worldIcon}>
+              <PublicIcon/>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className={classes.quantity}>
         <span>
           <LikeMiniIcon/>
--- src/frontend/src/components/Main/Feed/Post/styles.js
+++ src/frontend/src/components/Main/Feed/Post/styles.js
@@ -52,5 +52,55 @@ export default makeStyles(() => ({
         textDecoration: 'underline'
       }
     }
+  },
+  post: {
+    width: '45%',
+    border: '1px solid rgba(0, 0, 0, 0.5)',
+    borderRadius: '10px',
+    marginTop: '10px'
+  },
+  postAuthor: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  hiddenMenu: {
+    color: 'gray',
+    marginLeft: '95%',
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: 'rgba(0,0,0,0.10)',
+      borderRadius: '50%'
+    }
+  },
+  line: {
+    width: '95%',
+    height: '0.5px',
+    border: '0',
+    backgroundColor: '#e0e0e0'
+
+  },
+  userInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'baseline',
+    fontSize: 12,
+    fontWeight: 500
+  },
+  position: {
+    color: 'gray',
+    fontSize: 10
+  },
+  postTime: {
+    display: 'flex',
+    flexDirection: 'row',
+    color: 'gray',
+    fontSize: 10
+  },
+  worldIcon: {
+    color: 'gray',
+    '& > .MuiSvgIcon-root': {
+      fontSize: 12,
+      marginLeft: '5px'
+    }
   }
 }))
