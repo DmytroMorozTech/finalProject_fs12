9ebdb6546ff8efaac1859f82eddffcbe6c71f689
KostiantynRog <42935404+KostiantynRog@users.noreply.github.com>
2021-07-24 01:05:53 +0300

    Post author (#39)
    * part 1 of Post

--- src/frontend/src/components/Main/Feed/Post/styles.js
+++ src/frontend/src/components/Main/Feed/Post/styles.js
@@ -3,10 +3,13 @@ import { makeStyles } from '@material-ui/core/styles'
 export default makeStyles(() => ({
 
   post: {
+    padding: '0, 50px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    marginTop: '10px'
+    marginTop: '10px',
+    border: '1px solid rgba(0, 0, 0, 0.5)',
+    borderRadius: '10px'
   },
 
   picture: {
@@ -47,9 +50,7 @@ export default makeStyles(() => ({
     display: 'flex',
     alignItems: 'center',
     cursor: 'pointer',
-    borderBottom: '1px solid #e0e0e0',
     width: '100%',
-    paddingBottom: '5px',
     '& > .MuiSvgIcon-root': {
       fontSize: 3
     },
@@ -61,6 +62,54 @@ export default makeStyles(() => ({
       '&:hover': {
         textDecoration: 'underline'
       }
+    }
+  },
+  avatar: {
+    paddingLeft: '10px'
+  },
+  postAuthor: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  hiddenMenu: {
+    color: 'gray',
+    marginLeft: '90%',
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: 'rgba(0,0,0,0.10)',
+      borderRadius: '50%'
+    }
+  },
+  line: {
+    width: '95%',
+    height: '0.5px',
+    border: '0',
+    backgroundColor: '#e0e0e0'
+
+  },
+  userInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'baseline',
+    cursor: 'pointer',
+    fontSize: 12,
+    fontWeight: 500
+  },
+  position: {
+    color: 'gray',
+    fontSize: 10
+  },
+  postTime: {
+    display: 'flex',
+    flexDirection: 'row',
+    color: 'gray',
+    fontSize: 10
+  },
+  worldIcon: {
+    color: 'gray',
+    '& > .MuiSvgIcon-root': {
+      fontSize: 12,
+      marginLeft: '5px'
     }
   }
 }))
