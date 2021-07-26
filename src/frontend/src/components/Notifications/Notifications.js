0b22f85163d605f4d966867e5b7140df5711b849
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-07-26 17:00:11 +0300

    Added design Notifications page

--- src/frontend/src/components/Notifications/Notifications.js
+++ src/frontend/src/components/Notifications/Notifications.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Style from './styles'
+import Typography from '@material-ui/core/Typography'
+import TemporaryAvatar from '../../temporaryImages/avatarNotification.jpg'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
-function Notifications () {
+function Notifications ({
+  userAvatar = TemporaryAvatar,
+  userName = 'Fred Grint',
+  userAction = 'shared a post:',
+  userText = 'You can place a request in our Standard Product Catalogue following a few simple steps: ad your product to the cart, fill the contact form and complete your request.',
+  notificationTime = '2d'
+}) {
   const classes = Style()
   return (
-    <h1 className={classes.text}>
-        Welcome to the Notifications page!
-    </h1>
+    <div className={classes.notifications}>
+      <div className={classes.notification}>
+        <div>
+          <img src={userAvatar} alt={'user avatar'} className={classes.userAvatar}/>
+        </div>
+        <div>
+          <Typography variant="body1">
+            <span className={classes.userName}>{userName + ' '}</span>
+            <span className={classes.actionAndText}>{userAction + ' ' + userText}</span>
+          </Typography>
+        </div>
+        <div className={classes.notificationTimeAndMenu}>
+          <div className={classes.notificationTime}>
+            {notificationTime}
+          </div>
+          <div className={classes.menu}>
+            <MoreHorizIcon/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/frontend/src/components/Notifications/styles.js
+++ src/frontend/src/components/Notifications/styles.js
@@ -2,9 +2,57 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export default makeStyles(() => ({
 
-  text: {
+  notifications: {
+    maxWidth: '40%',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    border: '1px solid #e0e0e0',
+    borderRadius: '10px'
+  },
+
+  notification: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  userAvatar: {
+    width: '65px',
+    borderRadius: '50%',
+    margin: '15px'
+  },
+
+  userName: {
+    fontWeight: 600,
+    color: '#616161'
+  },
+
+  actionAndText: {
+    color: '#616161'
+  },
+
+  notificationTimeAndMenu: {
+    color: 'grey',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  notificationTime: {
+    fontSize: 12
+  },
+
+  menu: {
+    cursor: 'pointer',
+    width: '40px',
+    height: '40px',
+    '& > .MuiSvgIcon-root': {
+      margin: '8px'
+    },
+    '&:hover': {
+      backgroundColor: 'rgba(0,0,0,0.10)',
+      borderRadius: '50%'
+    }
   }
 }))
