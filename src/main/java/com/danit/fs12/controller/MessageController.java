1af71f298ec922c47dccf8917b99f10f6cebd884
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-07-30 14:24:25 +0300

    ChatService

--- src/main/java/com/danit/fs12/controller/MessageController.java
+++ src/main/java/com/danit/fs12/controller/MessageController.java
@@ -1,29 +1,46 @@
 package com.danit.fs12.controller;
 
+import com.danit.fs12.dto.message.MessageDtoRq;
+import com.danit.fs12.dto.message.MessageDtoRs;
+import com.danit.fs12.facade.MessageFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @Slf4j
-@RequestMapping("/api")
+@RequestMapping("/api/messages")
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
+  private final MessageFacade messageFacade;
 
-//  private final MessageService messageService;
-//  private final MessageFacade messageFacade;
+  @GetMapping
+  List<MessageDtoRs> findAll() {
+    return messageFacade.findAll();
+  }
 
-//  @GetMapping("/message")
-//  public void getMessage() {
-//
-//    return;
-//  }
-//
-//  @PostMapping("/message")
-//  public void postMessage() {
-//
-//    return;
-//  }
+  @GetMapping(path = "{id}")
+  public ResponseEntity<MessageDtoRs> findById(@PathVariable Long id) {
+    MessageDtoRs message = messageFacade.findById(id);
+    return ResponseEntity.ok(message);
+  }
+
+  @PostMapping
+  public ResponseEntity<?> createMessage(@Valid @RequestBody MessageDtoRq rq) {
+    Long activeUserId = rq.getActiveUserId();
+    Long chatId = rq.getChatId();
+    String text = rq.getText();
+
+    MessageDtoRs message = messageFacade.createMessage(activeUserId, chatId, text);
+    return ResponseEntity.ok(message);
+  }
 }
-
--- src/main/java/com/danit/fs12/facade/MessageFacade.java
+++ src/main/java/com/danit/fs12/facade/MessageFacade.java
@@ -3,9 +3,15 @@ package com.danit.fs12.facade;
 import com.danit.fs12.dto.message.MessageDtoRq;
 import com.danit.fs12.dto.message.MessageDtoRs;
 import com.danit.fs12.entity.Message;
+import com.danit.fs12.service.MessageService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageFacade extends GeneralFacade<Message, MessageDtoRq, MessageDtoRs> {
+  private MessageService messageService;
 
+  public MessageDtoRs createMessage(Long activeUserId, Long chatId, String text) {
+    Message message = messageService.createMessage(activeUserId, chatId, text);
+    return convertToDto(message);
+  }
 }
