1e5a6c06fa3e49354c45c73e35d6c1bc1c905d65
VadymMeshcheriakov <51048674+VadymMeshcheriakov@users.noreply.github.com>
2021-08-07 13:40:37 +0300

    Message (#71)
    - Designed Messages page.
    - Button is disabled when input is empty
    - Other elements also have some behaviour

---
+++ src/frontend/src/components/Main/Messages/imageUpload.js
@@ -0,0 +1,44 @@
+import React, { useState, useRef } from 'react'
+import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined'
+import Style from './styles'
+
+const ImageUpload = () => {
+  const classes = Style()
+  const [image, setImage] = useState('')
+  const inputFile = useRef(null)
+
+  const handleFileUpload = e => {
+    const { files } = e.target
+    if (files && files.length) {
+      const filename = files[0].name
+
+      let parts = filename.split('.')
+      const fileType = parts[parts.length - 1]
+      console.log('fileType', fileType) // ex: zip, rar, jpg, svg etc.
+
+      setImage(files[0])
+    }
+  }
+
+  const onButtonClick = () => {
+    inputFile.current.click()
+  }
+
+  console.log('imageimage', image)
+  return (
+    <div>
+      <input
+        style={{ display: 'none' }}
+        accept=".jpg, .jpeg, .png"
+        ref={inputFile}
+        onChange={handleFileUpload}
+        type="file"
+      />
+      <div className={classes.menu} onClick={onButtonClick}>
+        <ImageOutlinedIcon/>
+      </div>
+    </div>
+  )
+}
+
+export default ImageUpload
--- src/frontend/src/shared/Button/SharedButton.js
+++ src/frontend/src/shared/Button/SharedButton.js
@@ -1,11 +1,11 @@
 import Style from './styles'
 import Button from '@material-ui/core/Button'
 
-function SharedButton ({title, color = 'primary', variant = 'contained'}) {
+function SharedButton ({title, color = 'primary', variant = 'contained', disabled = false}) {
   const classes = Style()
   return (
     <div>
-      <Button color={color} variant={variant} className={classes.btn}>
+      <Button disabled = {disabled} color={color} variant={variant} className={classes.btn}>
         {title}
       </Button>
     </div>
