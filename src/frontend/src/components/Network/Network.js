1e03cb3babe966abc9b3f504b4c269c303a54f38
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-08-07 18:18:26 +0300

    - small fixes_2

--- src/frontend/src/components/Network/Network.js
+++ src/frontend/src/components/Network/Network.js
@@ -4,11 +4,9 @@ import Style from './styles'
 function Network () {
   const classes = Style()
   return (
-    <>
-      <h1 className={classes.text}>
+    <h1 className={classes.text}>
         Welcome to the Network page!
-      </h1>
-    </>
+    </h1>
   )
 }
 
