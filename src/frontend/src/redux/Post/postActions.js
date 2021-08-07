45b91b6a7a2fac69af4b7fe4fd470410493a586c
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-08-07 16:15:15 +0300

    - removed dependency unfetch; - refactored postsReducer using axios

--- src/frontend/src/redux/Post/postActions.js
+++ src/frontend/src/redux/Post/postActions.js
@@ -1,6 +1,4 @@
 import * as actions from './postActionTypes'
-import fetch from 'unfetch'
-import checkStatus from '../Services/checkStatus'
 import http from '../../services/httpService'
 
 export const getAllPostsAction = () => (dispatch) => {
@@ -16,59 +14,40 @@ export const getAllPostsAction = () => (dispatch) => {
 }
 
 export const createNewPostAction = (payload) => (dispatch) => {
-  return fetch(`/api/posts`,
-    {
-      headers: {'Content-Type': 'application/json'},
-      method: 'POST',
-      body: JSON.stringify(payload)
-    })
-    .then(checkStatus)
-    .then((res) => res.json())
-    .then((postObjFromServer) => {
-      dispatch({type: actions.ADD_NEW_POST, payload: postObjFromServer})
+  return http
+    .post('/api/posts', {text: payload.text})
+    .then((res) => res.data)
+    .then((newPostObj) => {
+      dispatch({type: actions.ADD_NEW_POST, payload: newPostObj})
     })
 }
 
 export const getCommentsForPostAction = (postId) => (dispatch) => {
-  return fetch(`/api/comments/for_post/${postId}`,
-    {
-      headers: {'Content-Type': 'application/json'},
-      method: 'GET'
-    })
-    .then(checkStatus)
-    .then((res) => res.json())
+  return http
+    .get(`/api/comments/for_post/${postId}`)
+    .then((res) => res.data)
     .then((listOfComments) => {
       dispatch({type: actions.SAVE_COMMENTS_FOR_POST, payload: {listOfComments, postId}})
     })
 }
 
 export const createNewCommentAction = ({text, id}) => (dispatch) => {
-  return fetch(`/api/comments`,
-    {
-      headers: {'Content-Type': 'application/json'},
-      method: 'POST',
-      body: JSON.stringify({ text, postId: id })
-    })
-    .then(checkStatus)
-    .then((res) => res.json())
-    .then((commentObjFromServer) => {
-      dispatch(
-        {
-          type: actions.ADD_NEW_COMMENT_FOR_POST,
-          payload: { comment: commentObjFromServer, postId: id }
-        })
+  return http
+    .post('/api/comments', {text: text, postId: id})
+    .then((res) => res.data)
+    .then((newCommentObj) => {
+      dispatch({
+        type: actions.ADD_NEW_COMMENT_FOR_POST,
+        payload: { comment: newCommentObj, postId: id }
+      })
     })
 }
 
 export const getUsersWhoLikedPostAction = (payload) => (dispatch) => {
   const id = payload
-  return fetch(`/api/users/who_liked_post/${id}`,
-    {
-      headers: {'Content-Type': 'application/json'},
-      method: 'GET'
-    })
-    .then(checkStatus)
-    .then((res) => res.json())
+  return http
+    .get(`/api/users/who_liked_post/${id}`)
+    .then((res) => res.data)
     .then((usersList) => {
       dispatch({type: actions.SAVE_USERS_WHO_LIKED_POST, payload: {usersList, id}})
     })
@@ -76,16 +55,11 @@ export const getUsersWhoLikedPostAction = (payload) => (dispatch) => {
 
 export const toggleLikeAction = (payload) => (dispatch) => {
   const id = payload
-  // console.log(`toggleLikeAction with postId: ${payload}`)
 
-  return fetch(`/api/posts/toggle_like/${id}`,
-    {
-      headers: {'Content-Type': 'application/json'},
-      method: 'POST'
-    })
-    .then(checkStatus)
-    .then((res) => res.json())
-    .then((postObjFromServer) => {
-      dispatch({type: actions.UPDATE_POST, payload: postObjFromServer})
+  return http
+    .post(`/api/posts/toggle_like/${id}`)
+    .then((res) => res.data)
+    .then((newPostObj) => {
+      dispatch({type: actions.UPDATE_POST, payload: newPostObj})
     })
 }
--- src/main/java/com/danit/fs12/entity/post/PostRq.java
+++ src/main/java/com/danit/fs12/entity/post/PostRq.java
@@ -10,7 +10,6 @@ public class PostRq {
 
   @Size(min = 1, max = 1000, message = "Length of post message should be within range 1-1000 characters")
   // on original LinkedIn the maxLength of post text field is 1300 characters
-
   @NotNull(message = "Post text should be specified")
   String text;
 
