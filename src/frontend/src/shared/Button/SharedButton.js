bf1ae5962bc75a0628913d2f6cdb2091033114ce
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-08-08 13:20:59 +0300

    Small fixes

--- src/frontend/src/shared/Button/SharedButton.js
+++ src/frontend/src/shared/Button/SharedButton.js
@@ -1,10 +1,10 @@
 import styles from './styles'
 import Button from '@material-ui/core/Button'
 
-function SharedButton ({ title, color = 'primary', variant = 'contained', disabled = false, size = 'small' }) {
+function SharedButton ({ title, color = 'primary', variant = 'contained', disabled = false, size = 'small', onClick }) {
   const classes = styles()
   return (
-    <Button color={color} variant={variant} disabled={disabled} size={size} className={classes.btn}>
+    <Button color={color} variant={variant} disabled={disabled} size={size} className={classes.btn} onClick={onClick}>
       {title}
     </Button>
   )
