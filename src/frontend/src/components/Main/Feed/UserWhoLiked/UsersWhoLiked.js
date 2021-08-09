9d73307c620cb855fe3ee4572edf239626f967b5
krozhkov09@gmail.com <krozhkov09@gmail.com>
2021-08-09 20:52:04 +0300

    - small fixes

--- src/frontend/src/components/Main/Feed/UserWhoLiked/UsersWhoLiked.js
+++ src/frontend/src/components/Main/Feed/UserWhoLiked/UsersWhoLiked.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import {connect, useDispatch} from 'react-redux'
 import {withStyles} from '@material-ui/core/styles'
 import MuiDialogTitle from '@material-ui/core/DialogTitle'
 import Typography from '@material-ui/core/Typography'
@@ -7,12 +7,21 @@ import Style from './styles'
 import UserWhoLikedItem from './UserWhoLikedItem'
 import clsx from 'clsx'
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import toggleModalAction from '../../../../redux/Modal/modalActions'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
 
 const styles = (theme) => ({
   root: {
     width: '500px',
     margin: 0,
     padding: theme.spacing(2)
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500]
   }
 })
 
@@ -21,6 +30,11 @@ const DialogTitle = withStyles(styles)((props) => {
   return (
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
       <Typography variant="h6">{children}</Typography>
+      {onClose && (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon/>
+        </IconButton>
+      )}
     </MuiDialogTitle>
   )
 })
@@ -28,10 +42,14 @@ const UsersWhoLiked = (props) => {
   const {usersWhoLikedPost, postId} = props
   const usersWhoLikedThisPost = usersWhoLikedPost[postId]
   const classes = Style()
+  const dispatch = useDispatch()
+  const onPostSubmitHandler = () => {
+    dispatch(toggleModalAction())
+  }
 
   return (
     <div>
-      <DialogTitle id="customized-dialog-title">
+      <DialogTitle id="customized-dialog-title" onClose={onPostSubmitHandler}>
         <div className={classes.title}>
           <Typography variant="body1" className={clsx(classes.word)}>
             <p>All</p>
