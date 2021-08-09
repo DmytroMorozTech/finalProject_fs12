23e18ac9c055322a1cb9c00df61c0b7ebe122e4b
krozhkov09@gmail.com <krozhkov09@gmail.com>
2021-08-09 10:58:07 +0300

    Cleaned imports in two components ShareBox.js and UsersWhoLiked.js

--- src/frontend/src/components/Main/Feed/ShareBox/ShareBox.js
+++ src/frontend/src/components/Main/Feed/ShareBox/ShareBox.js
@@ -7,7 +7,7 @@ import AssignmentIcon from '@material-ui/icons/Assignment'
 import Avatar from '../../../../shared/Avatar/Avatar'
 import toggleModalAction from '../../../../redux/Modal/modalActions'
 import {useDispatch} from 'react-redux'
-import {ADD_NEW_POST, SHOW_USERS_WHO_LIKED_POST} from '../../../Modal/modalTypes'
+import {ADD_NEW_POST} from '../../../Modal/modalTypes'
 import {Hidden} from '@material-ui/core'
 
 function ShareBox (props) {
