a6b9c84a4d6c1f1ada3a6e282531135a82b095c1
Serhii <romaniuk87@gmail.com>
2021-08-10 23:12:22 +0300

    -- init

---
+++ src/main/java/com/danit/fs12/entity/certification/CertificationRs.java
@@ -0,0 +1,21 @@
+package com.danit.fs12.entity.certification;
+
+import com.danit.fs12.entity.user.UserRs;
+import lombok.Data;
+
+import javax.persistence.Column;
+import java.time.LocalDate;
+
+@Data
+public class CertificationRs {
+
+  private String id;
+  private String name;
+  private String issuingOrganization;
+  private Boolean hasExpiryDate;
+  private LocalDate issueDate;
+  private LocalDate expirationDate;
+  private String credentialId;
+  private String credentialUrl;
+  private UserRs userRs;
+}
