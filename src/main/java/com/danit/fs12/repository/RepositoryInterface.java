700847288d5159020467320b2907bd3131a37513
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-08-10 21:55:54 +0300

    - small fix

--- src/main/java/com/danit/fs12/repository/RepositoryInterface.java
+++ src/main/java/com/danit/fs12/repository/RepositoryInterface.java
@@ -7,12 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RepositoryInterface<E extends AbstractEntity> extends JpaRepository<E, Long> {
-  default E findEntityById(Long id){
-      Optional<E> entityOptional = findById(id);
-      if (entityOptional.isEmpty()) {
-        String msg = String.format("An error occurred while trying to find post with id %d. ", id);
-        throw new BadRequestException(msg);
-      }
-      return entityOptional.get();
+  default E findEntityById(Long id) {
+    Optional<E> entityOptional = findById(id);
+    if (entityOptional.isEmpty()) {
+
+      String msg = String.format("An error occurred while trying to find entity with id %d. ", id);
+      throw new BadRequestException(msg);
+    }
+    return entityOptional.get();
   }
 }
