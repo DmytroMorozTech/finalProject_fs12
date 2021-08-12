cce32fa2f3123c4c876592f5368d5c4f0bac5368
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-08-12 16:16:42 +0300

    - completed infinite scroll functionality on Feed with Posts

--- src/frontend/src/components/Main/Feed/Feed.js
+++ src/frontend/src/components/Main/Feed/Feed.js
@@ -2,28 +2,92 @@ import React, { useEffect } from 'react'
 import styles from './styles'
 import Post from './Post/Post'
 import ShareBox from './ShareBox/ShareBox'
-import { connect } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
+import Preloader from '../../../shared/Preloader/Preloader'
+import * as actions from '../../../redux/Post/postActionTypes'
+import http from '../../../services/httpService'
 
 function Feed (props) {
-  const { posts } = props
+  const { posts, pageNumber, pageSize, hasMore = true, loading = true, sortBy } = props
+  const dispatch = useDispatch()
   const classes = styles()
 
+  const load = () => {
+    console.log(`Load function has triggered!`)
+    dispatch({ type: actions.LOADING_POSTS, payload: true })
+
+    return http
+      .get('api/posts',
+        {
+          params: {
+            pageNumber: pageNumber,
+            pageSize: pageSize,
+            sortBy: sortBy // so far this property is not used
+          }
+        }
+      )
+      .then((result) => result.data)
+      .then((page) => {
+        dispatch({ type: actions.SAVE_NEW_POSTS, payload: page })
+        dispatch({ type: actions.LOADING_POSTS, payload: false })
+      })
+  }
+
+  const loader = React.useRef(load)
+
+  const observer = React.useRef(
+    new IntersectionObserver(
+      entries => {
+        const first = entries[0]
+        if (first.isIntersecting) {
+          loader.current()
+        }
+      },
+      { threshold: 1, rootMargin: '50px' }
+    )
+  )
+  const [element, setElement] = React.useState(null)
+
   useEffect(() => {
-  }, [posts])
-  // this useEffect is needed to rerender Feed component if some data related to accounts changes in Redux store.
+    loader.current = load
+  }, [load])
+
+  useEffect(() => {
+    const currentElement = element
+    const currentObserver = observer.current
+
+    if (currentElement) {
+      currentObserver.observe(currentElement)
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement)
+      }
+    }
+  }, [element])
 
   return (
-    <div className={classes.feed}>
-      <ShareBox/>
-      {posts.map(post => <Post key={post.id} post={post}/>)}
-    </div>
+    <>
+      <div className={classes.feed}>
+        <ShareBox/>
+        {posts.map(post => <Post key={post.id} post={post}/>)}
+      </div>
+
+      {loading && <Preloader/>}
+      {!loading && hasMore && <p ref={setElement} style={{ background: 'transparent' }}></p>}
+    </>
   )
 }
 
 const mapStateToProps = (state) => {
   return {
-    activeUserId: state.user.activeUserId,
-    posts: state.posts.postsList
+    activeUserId: state.user.activeUser.id,
+    posts: state.posts.postsList,
+    loading: state.posts.loading,
+    pageNumber: state.posts.pageNumber,
+    pageSize: state.posts.pageSize,
+    hasMore: state.posts.hasMore
   }
 }
 
