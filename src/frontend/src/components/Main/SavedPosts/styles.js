6a2d18bbc2e37fce959204c05bf2c8c25b8aa755
Olegio <ogorski23@gmail.com>
2021-08-14 16:34:41 +0300

    - Changed position of Button "All"

--- src/frontend/src/components/Main/SavedPosts/styles.js
+++ src/frontend/src/components/Main/SavedPosts/styles.js
@@ -10,7 +10,7 @@ export default makeStyles((theme) => ({
     padding: '8px 12px',
     backgroundColor: 'transparent',
     color: theme.palette.grey[600],
-    marginLeft: '21.5px'
+    marginLeft: '21.4px'
   },
   savedElementsPanel: {
     border: theme.border.simple,
