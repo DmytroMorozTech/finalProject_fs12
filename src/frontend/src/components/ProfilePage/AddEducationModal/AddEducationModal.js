72948dae734003f97691d2c6d0f118a082290556
krozhkov09@gmail.com <krozhkov09@gmail.com>
2021-08-15 22:51:38 +0300

    added type="submit" to SharedButton.js

--- src/frontend/src/components/ProfilePage/AddEducationModal/AddEducationModal.js
+++ src/frontend/src/components/ProfilePage/AddEducationModal/AddEducationModal.js
@@ -4,7 +4,7 @@ import MuiDialogContent from '@material-ui/core/DialogContent'
 import MuiDialogActions from '@material-ui/core/DialogActions'
 import styles from './styles'
 import Typography from '@material-ui/core/Typography'
-import {Form, Formik, useFormikContext} from 'formik'
+import {Form, Formik} from 'formik'
 import * as Yup from 'yup'
 import TextField from '../../../shared/FormComponents/TextField/TextField'
 import Select from '../../../shared/FormComponents/Select/Select'
@@ -29,13 +29,6 @@ const DialogActions = withStyles((theme) => ({
 }))(MuiDialogActions)
 
 const AddEducationModal = () => {
-  // const {submitForm} = useFormikContext()
-  // const handleSubmit = () => {
-  //   submitForm()
-  // }
-  // const configSharedButton = {
-  //   onClick: handleSubmit
-  // }
   const classes = styles()
   const INITIAL_FORM_STATE = {
     school: '',
@@ -69,98 +62,93 @@ const AddEducationModal = () => {
       </Typography>
      
       <Grid container>
-        <Grid item xs={12}>
-          <div className={classes.formWrapper}>
-            <Formik
-              initialValues={{
-                ...INITIAL_FORM_STATE
-              }}
-              validationSchema={FORM_VALIDATION}
-              onSubmit={values => {
-                console.log(values)
-              }}
-            >
-              <Form>
-                <DialogContent dividers>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="school"
-                        label="School"
-                        helperText="Ex: Boston University"
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="degree"
-                        label="Degree"
-                        helperText="Ex: Bachelor`s"
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="fieldOfStudy"
-                        label="Field of study"
-                        helperText="Ex: Business"
-                      />
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Select
-                        name="startMonth"
-                        label="Month"
-                        options={month}
-                        helperText="Start date"
-                      />
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Select
-                        name="startYear"
-                        label="Year"
-                        options={year}
-                      />
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Select
-                        name="endMonth"
-                        label="Month"
-                        options={month}
-                        helperText="End date"
-                      />
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Select
-                        name="endDate"
-                        label="Year"
-                        options={year}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="activitiesAndSocieties"
-                        label="Activities and societies"
-                        multiline={true}
-                        row={2}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        name="description"
-                        label="Description"
-                        multiline={true}
-                        row={4}
-                      />
-                    </Grid>
-                  </Grid>
-                </DialogContent>
-                <DialogActions>
-                  <SharedButton title="Save"/>
-                </DialogActions>
-              </Form>
-            </Formik>
-          </div>
-        </Grid>
+        <Formik
+          initialValues={{
+            ...INITIAL_FORM_STATE
+          }}
+          validationSchema={FORM_VALIDATION}
+          onSubmit={values => {
+            console.log(values)
+          }}
+        >
+          <Form>
+            <DialogContent dividers>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    name="school"
+                    label="School"
+                    helperText="Ex: Boston University"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="degree"
+                    label="Degree"
+                    helperText="Ex: Bachelor`s"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="fieldOfStudy"
+                    label="Field of study"
+                    helperText="Ex: Business"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Select
+                    name="startMonth"
+                    label="Month"
+                    options={month}
+                    helperText="Start date"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Select
+                    name="startYear"
+                    label="Year"
+                    options={year}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Select
+                    name="endMonth"
+                    label="Month"
+                    options={month}
+                    helperText="End date"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Select
+                    name="endDate"
+                    label="Year"
+                    options={year}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="activitiesAndSocieties"
+                    label="Activities and societies"
+                    multiline={true}
+                    row={2}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="description"
+                    label="Description"
+                    multiline={true}
+                    row={4}
+                  />
+                </Grid>
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <SharedButton title="Save"/>
+            </DialogActions>
+          </Form>
+        </Formik>
       </Grid>
-     
     </div>
   )
 }
