89d6ac74010426741b24af083a6a7251fe27cf05
OksanaMarysheva <87086991+OksanaMarysheva@users.noreply.github.com>
2021-08-18 21:50:57 +0300

    Design LeftTab on Jobs Page (#110)
    * Added Grid to Jobs
    
    * Added design of LeftTab on Jobs Page

---
+++ src/frontend/src/components/Jobs/JobsLeft/JobsLeft.js
@@ -0,0 +1,34 @@
+import styles from './styles'
+import React from 'react'
+import BookmarkIcon from '@material-ui/icons/Bookmark'
+import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded'
+import LocalAtmIcon from '@material-ui/icons/LocalAtm'
+import AssignmentTurnedInOutlinedIcon from '@material-ui/icons/AssignmentTurnedInOutlined'
+import NoteIcon from '@material-ui/icons/Note'
+import InsertDriveFileOutlinedIcon from '@material-ui/icons/InsertDriveFileOutlined'
+import SettingsIcon from '@material-ui/icons/Settings'
+import Item from './Item/Item'
+
+function JobsLeft () {
+  const classes = styles()
+
+  const items = [
+    { Icon: <BookmarkIcon fontSize="inherit"/>, title: 'My Jobs', onClick: () => console.log('My Jobs') },
+    { Icon: <NotificationsRoundedIcon fontSize="inherit"/>, title: 'Job Alerts', onClick: () => console.log('Job Alerts') },
+    { Icon: <LocalAtmIcon fontSize="inherit"/>, title: 'Salary', onClick: () => console.log('Salary') },
+    { Icon: <AssignmentTurnedInOutlinedIcon fontSize="inherit"/>, title: 'Skill Assessments', onClick: () => console.log('Skill Assessments') },
+    { Icon: <NoteIcon fontSize="inherit"/>, title: 'Interview Prep', onClick: () => console.log('Interview Prep') },
+    { Icon: <InsertDriveFileOutlinedIcon fontSize="inherit"/>, title: 'Resume Builder', onClick: () => console.log('Resume Builder') },
+    { Icon: <SettingsIcon fontSize="inherit"/>, title: 'Application Settings', onClick: () => console.log('Application Settings') }
+  ]
+
+  return (
+    <div className={classes.root}>
+      {items.map(({ Icon, title, onClick }, i) => (
+        <Item key={i} Icon={Icon} title={title} onClick={onClick}/>
+      ))}
+    </div>
+  )
+}
+
+export default JobsLeft
