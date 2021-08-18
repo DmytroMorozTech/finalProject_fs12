e9bc631f7fc8dcfbc9b0f4a90e380e091c94bc18
Serhii <romaniuk87@gmail.com>
2021-08-18 09:18:08 +0300

    -- small fixes

--- src/main/java/com/danit/fs12/security/config/BcryptPassEncoderConfig.java
+++ src/main/java/com/danit/fs12/security/config/BcryptPassEncoderConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class BCryptPassEncoderConfig {
+public class BcryptPassEncoderConfig {
 
   @Bean
   public BCryptPasswordEncoder bCrypt() {
