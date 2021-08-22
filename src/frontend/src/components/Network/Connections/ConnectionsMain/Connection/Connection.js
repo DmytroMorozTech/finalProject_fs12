a33548ed532f8117b94ce3c676db3430483485c6
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-08-22 16:46:04 +0300

    Added design of ConnectionsMain

---
+++ src/frontend/src/components/Network/Connections/ConnectionsMain/Connection/Connection.js
@@ -0,0 +1,13 @@
+import styles from './styles'
+
+function Connection () {
+  const classes = styles()
+
+  return (
+    <div className={classes.connection}>
+
+    </div>
+  )
+}
+
+export default Connection
