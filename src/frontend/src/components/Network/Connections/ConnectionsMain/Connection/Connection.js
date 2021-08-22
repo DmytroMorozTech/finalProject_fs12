81b636add7afb48c15c8fa40366325f21e5b0b57
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-08-22 19:53:21 +0300

    Added design of Connection

--- src/frontend/src/components/Network/Connections/ConnectionsMain/Connection/Connection.js
+++ src/frontend/src/components/Network/Connections/ConnectionsMain/Connection/Connection.js
@@ -1,11 +1,55 @@
 import styles from './styles'
+import avatar from '../../../../../temporaryImages/avatar.jpg'
+import Avatar from '../../../../../shared/Avatar/Avatar'
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import SharedButton from '../../../../../shared/Button/SharedButton'
+import TreeDots from '../../../../../shared/ThreeDots/TreeDots'
+import { Link } from 'react-router-dom'
 
-function Connection () {
+function Connection (props) {
   const classes = styles()
 
-  return (
-    <div className={classes.connection}>
+  const {
+    avatarUrl = avatar,
+    fullName = 'George Lupin',
+    positionAndCompany = 'Fullstack Developer — GlobalLogic',
+    dateCreated = '3 days ago'
+  } = props
 
+  return (
+    <div>
+      <div>
+        <div className={classes.connection}>
+          <div className={classes.flex}>
+            <Link to="/profiles/:id">
+              <div className={classes.avatar}>
+                <Avatar avatarUrl={avatarUrl}/>
+              </div>
+            </Link>
+            <div className={classes.userInfo}>
+              <Link to="/profiles/:id" className={classes.link}>
+                {fullName}
+              </Link>
+              <Typography variant="body1" color="textSecondary">
+                {positionAndCompany}
+              </Typography>
+              <Typography variant="h6" color="textSecondary">
+                Connected {dateCreated}
+              </Typography>
+            </div>
+          </div>
+          <div className={classes.buttons}>
+            <div>
+              <SharedButton title="Message" size='medium' variant="outlined"/>
+            </div>
+            <div>
+              <TreeDots/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className={classes.line}/>
     </div>
   )
 }
--- src/frontend/src/components/Network/Connections/ConnectionsMain/Connection/styles.js
+++ src/frontend/src/components/Network/Connections/ConnectionsMain/Connection/styles.js
@@ -3,6 +3,53 @@ import { makeStyles } from '@material-ui/core/styles'
 export default makeStyles((theme) => ({
 
   connection: {
+    display: 'flex',
+    marginLeft: theme.spacing(4),
+    marginRight: theme.spacing(4),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    justifyContent: 'space-between'
+  },
 
+  flex: {
+    display: 'flex'
+  },
+
+  avatar: {
+    width: theme.avatar.large,
+    borderRadius: '50%'
+  },
+
+  userInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginLeft: theme.spacing(2)
+  },
+
+  link: {
+    textDecoration: 'none',
+    fontSize: theme.typography.sh3.fontSize,
+    fontWeight: theme.typography.fontWeightBold,
+    color: theme.palette.grey[700],
+    '&:hover': {
+      textDecoration: 'underline',
+      textDecorationThickness: '2px',
+      textDecorationColor: theme.palette.grey[700]
+    }
+  },
+
+  buttons: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+
+  line: {
+    width: 'calc(100%-100px)',
+    height: '0.5px',
+    border: '0',
+    margin: '0',
+    marginLeft: '100px',
+    backgroundColor: theme.palette.grey[300]
   }
 }))
