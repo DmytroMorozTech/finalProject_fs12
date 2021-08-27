aa6b2772e3e8c3ec1c3d5284458e18701ae84719
Dmytro Moroz <dmytro.moroz3@gmail.com>
2021-08-27 17:41:15 +0300

    - chronological validation for form inputs was implemented using YUP (startDate can not go after the endDate); - this functionality was implemented for such entities: Education, Certification, WorkPlace (Experience) both for adding these entities and for editing them at api/profiles page.

--- src/frontend/src/components/ProfilePage/AddEducationModal/AddEducationModal.js
+++ src/frontend/src/components/ProfilePage/AddEducationModal/AddEducationModal.js
@@ -15,6 +15,7 @@ import SharedButton from '../../../shared/Button/SharedButton'
 import {useDispatch} from 'react-redux'
 import toggleModalAction from '../../../redux/Modal/modalActions'
 import {createNewEducationAction} from '../../../redux/Profile/profileActions'
+import convertYearMonthToLocalDate from '../../../utils/convertYearMonthToLocalDate'
 
 const DialogContent = withStyles((theme) => ({
   root: {
@@ -58,9 +59,33 @@ const AddEducationModal = () => {
     startYear: Yup.number()
       .required('Required'),
     endMonth: Yup.string()
-      .required('Required'),
+      .required('Required')
+      .test(
+        'chronological_order',
+        'You should follow chronological order',
+        function () {
+          const {startMonth, startYear, endMonth, endYear} = this.parent
+          if (!endMonth || !endYear) return true
+          const startDateStr = convertYearMonthToLocalDate(startYear, startMonth)
+          const endDateStr = convertYearMonthToLocalDate(endYear, endMonth)
+          const startDateMillis = Date.parse(startDateStr)
+          const endDateMillis = Date.parse(endDateStr)
+          return (endDateMillis - startDateMillis) > 0
+        }),
     endYear: Yup.number()
-      .required('Required'),
+      .required('Required')
+      .test(
+        'chronological_order',
+        'You should follow chronological order',
+        function () {
+          const {startMonth, startYear, endMonth, endYear} = this.parent
+          if (!endMonth || !endYear) return true
+          const startDateStr = convertYearMonthToLocalDate(startYear, startMonth)
+          const endDateStr = convertYearMonthToLocalDate(endYear, endMonth)
+          const startDateMillis = Date.parse(startDateStr)
+          const endDateMillis = Date.parse(endDateStr)
+          return (endDateMillis - startDateMillis) > 0
+        }),
     activities: Yup.string()
       .required('Required'),
     description: Yup.string()
