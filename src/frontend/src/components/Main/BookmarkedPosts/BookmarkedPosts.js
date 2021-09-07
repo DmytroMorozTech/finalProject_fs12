6d9aaec54d5173070b3bc9a0b1d06299710d4f8a
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-09-08 02:57:06 +0300

    Fixed BookmarkedPosts Page and styles.

--- src/frontend/src/components/Main/BookmarkedPosts/BookmarkedPosts.js
+++ src/frontend/src/components/Main/BookmarkedPosts/BookmarkedPosts.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles'
-import { Container } from '@material-ui/core'
+import { Container, Typography } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid'
 import BookmarkIcon from '@material-ui/icons/Bookmark'
 import Feed from '../Feed/Feed'
@@ -10,14 +10,16 @@ function BookmarkedPosts () {
 
   return (
     <Container className={classes.container} maxWidth={'lg'}>
-      <Grid container spacing={4} alignItems={'flex-start'} justifyContent={'center'}>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+      <Grid container spacing={4} justifyContent={'center'}>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
           <div className={classes.savedElementsPanel}>
-            <h3 className={classes.savedElementsHeading}>
+            <Typography variant='h3' className={classes.savedElementsHeading}>
               <BookmarkIcon/>
               Saved Items
-            </h3>
-            <p className={classes.savedElementsSubheading}>Anything you save is private.</p>
+            </Typography>
+            <Typography variant='h5' className={classes.savedElementsSubheading}>
+              Anything you save is private.
+            </Typography>
           </div>
           <Feed type={'bookmarkedPosts'}/>
         </Grid>
--- src/frontend/src/components/Main/BookmarkedPosts/styles.js
+++ src/frontend/src/components/Main/BookmarkedPosts/styles.js
@@ -4,31 +4,23 @@ export default makeStyles((theme) => ({
   container: {
     marginTop: '75px'
   },
-  savedElementsBtnAll: {
-    borderRadius: '1.99rem',
-    fontSize: '1.6rem',
-    padding: '8px 12px',
-    backgroundColor: 'transparent',
-    color: theme.palette.grey[600],
-    marginLeft: '21.4px'
-  },
+
   savedElementsPanel: {
     border: theme.border.simple,
     borderRadius: theme.shape.medium,
     backgroundColor: theme.palette.common.white,
-    marginBottom: theme.spacing(3)
+    padding: theme.spacing(2),
+    paddingLeft: theme.spacing(4)
   },
+
   savedElementsHeading: {
     display: 'flex',
     alignItems: 'center',
-    color: theme.palette.grey[500],
-    fontSize: '18px',
-    paddingLeft: '35px',
-    fontWeight: 'bold'
+    color: theme.palette.grey[700]
   },
+
   savedElementsSubheading: {
-    color: theme.palette.grey[500],
-    fontSize: '15px',
-    paddingLeft: '35px'
+    paddingLeft: theme.spacing(1),
+    paddingTop: theme.spacing(1)
   }
 }))
