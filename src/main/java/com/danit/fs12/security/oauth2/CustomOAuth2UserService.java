ced2bab3b5c71c8d1ccfc86060f700dfa8e6b018
Serhii <romaniuk87@gmail.com>
2021-09-07 15:25:04 +0300

    -- small fixes

--- src/main/java/com/danit/fs12/security/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/danit/fs12/security/oauth2/CustomOAuth2UserService.java
@@ -9,7 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-  //here we override the loadUser() method which will be called by Spring OAuth2 upon successful auth and it returns a new CustomOAuth2User object
+  //here we override the loadUser() method which will be called
+  // by Spring OAuth2 upon successful auth and it returns a new CustomOAuth2User object
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
     OAuth2User user = super.loadUser(userRequest);
