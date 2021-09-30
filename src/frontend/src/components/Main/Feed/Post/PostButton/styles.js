0057eb12f0a8ec45f292b3c7d7db3a4381140fed
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-09-30 15:08:58 +0300

    Fixed styles

--- src/frontend/src/components/Main/Feed/Post/PostButton/styles.js
+++ src/frontend/src/components/Main/Feed/Post/PostButton/styles.js
@@ -10,17 +10,21 @@ export default makeStyles((theme) => ({
     display: 'flex',
     color: theme.palette.grey[600],
     marginLeft: theme.spacing(2),
-    marginTop: theme.spacing(1)
+    marginRight: theme.spacing(2),
+    marginTop: theme.spacing(1),
+    width: '96%'
   },
 
   item: {
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
     cursor: 'pointer',
     transition: 'all 0.35s ease',
     padding: theme.spacing(2),
     borderRadius: theme.shape.small,
     fontSize: theme.typography.icons.large.fontSize,
+    width: '25%',
     '&:hover': {
       backgroundColor: 'rgba(0,0,0,0.10)'
     },
