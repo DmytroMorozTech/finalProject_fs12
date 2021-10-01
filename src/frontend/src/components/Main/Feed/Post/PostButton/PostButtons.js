b2cafbc93a366b207ed40ecccd3548a3051e86e3
OksanaMarysheva <87086991+OksanaMarysheva@users.noreply.github.com>
2021-10-01 13:01:08 +0300

    Implemented copy link button in post (#187)
    * Fixed styles
    
    * Implemented copy link on pressed button 'Copy link' in post.

--- src/frontend/src/components/Main/Feed/Post/PostButton/PostButtons.js
+++ src/frontend/src/components/Main/Feed/Post/PostButton/PostButtons.js
@@ -1,10 +1,10 @@
 import { togglePostLikeGeneralAction, togglePostLikeSingleAction } from '../../../../../redux/Post/postActions'
 import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined'
-import { Hidden } from '@material-ui/core'
+import { Hidden, Tooltip } from '@material-ui/core'
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined'
 import RedoOutlinedIcon from '@material-ui/icons/RedoOutlined'
 import TelegramIcon from '@material-ui/icons/Telegram'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles'
 import { useDispatch } from 'react-redux'
 import clsx from 'clsx'
@@ -17,6 +17,18 @@ function PostButtons (props) {
 
   const dispatch = useDispatch()
 
+  const linkToPost = 'http://localhost:3000/posts/' + postId
+
+  const [open, setOpen] = useState(false)
+
+  const handleTooltipClose = () => {
+    setOpen(false)
+  }
+
+  const handleTooltipOpen = () => {
+    setOpen(true)
+  }
+
   const handleLikeGeneral = () => {
     dispatch(togglePostLikeGeneralAction(postId))
   }
@@ -27,8 +39,9 @@ function PostButtons (props) {
 
   const handleLike = singlePostRender ? handleLikeSingle : handleLikeGeneral
 
-  const handleShare = () => {
-    // TODO: handleShare
+  const handleCopy = () => {
+    navigator.clipboard.writeText(linkToPost)
+    handleTooltipOpen()
   }
 
   const handleSend = () => {
@@ -38,7 +51,7 @@ function PostButtons (props) {
   const items = [
     { icon: <ThumbUpOutlinedIcon fontSize="inherit"/>, title: 'Like', onClick: handleLike, className: clsx(classes.item, isLikedByActiveUser && classes.liked) },
     { icon: <ChatOutlinedIcon fontSize="inherit"/>, title: 'Comment', onClick: handleComment, className: classes.item },
-    { icon: <RedoOutlinedIcon fontSize="inherit"/>, title: 'Share', onClick: handleShare, className: classes.item },
+    { icon: <RedoOutlinedIcon fontSize="inherit"/>, title: 'Copy link', onClick: handleCopy, className: classes.item },
     { icon: <TelegramIcon fontSize="inherit"/>, title: 'Send', onClick: handleSend, className: classes.item }
   ]
 
@@ -46,12 +59,24 @@ function PostButtons (props) {
     <div className={classes.postButtonsWrapper}>
       <div className={classes.block}>
         {items.map(({ icon, title, onClick, className }, i) => (
-          <div key={i} className={className} onClick={onClick}>
-            {icon}
-            <Hidden xsDown>
-              <span>{title}</span>
-            </Hidden>
-          </div>
+          <Tooltip
+            PopperProps={{
+              disablePortal: true
+            }}
+            onClose={handleTooltipClose}
+            open={title === 'Copy link' ? open : ''}
+            title="Link copied"
+            placement="top"
+          >
+
+            <div key={i} className={className} onClick={onClick}>
+              {icon}
+              <Hidden xsDown>
+                <span>{title}</span>
+              </Hidden>
+            </div>
+
+          </Tooltip>
         ))}
       </div>
       <div className={showedAddComment ? classes.showedAddComment : classes.hidden}>
