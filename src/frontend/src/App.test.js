25c7c871ca2ac1736a63fdf62fa9abf785a946e1
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-10-12 09:29:41 +0300

    Fixed configuration of tests.

--- src/frontend/src/App.test.js
+++ src/frontend/src/App.test.js
@@ -3,7 +3,6 @@ import React from 'react'
 import App from './App'
 import { shallow } from 'enzyme'
 import * as redux from 'react-redux'
-import { getActiveUserAction } from './redux/User/userActions'
 
 const setUp = (props) => shallow(<App {...props} />)
 
@@ -35,5 +34,32 @@ describe('should render App component', () => {
 
   it('should work dispatch', () => {
     expect(mockDispatch).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('should render App component', () => {
+  let spyOnUseSelector
+  let spyOnUseDispatch
+  let mockDispatch
+
+  let component
+
+  beforeEach(() => {
+    spyOnUseSelector = jest.spyOn(redux, 'useSelector')
+    spyOnUseSelector.mockReturnValue(true)
+    spyOnUseDispatch = jest.spyOn(redux, 'useDispatch')
+    mockDispatch = jest.fn()
+    spyOnUseDispatch.mockReturnValue(mockDispatch)
+
+    component = setUp()
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should contain Preloader wrapper', () => {
+    const wrapper = component.find('Preloader')
+    expect(wrapper.length).toBe(1)
   })
 })
--- src/frontend/src/shared/PopupMenu/PopupMenu.test.js
+++ src/frontend/src/shared/PopupMenu/PopupMenu.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PopupMenu from './PopupMenu'
-import { render, mount } from 'enzyme'
+import { render } from 'enzyme'
 import theme from '../../Theme/Theme'
 import { ThemeProvider } from '@material-ui/styles'
 
@@ -21,12 +21,19 @@ describe('should render PopupMenu component', () => {
 //   let component
 //   beforeEach(() => {
 //     component = mount(<PopupMenu/>)
-//     it('user click span', () => {
-//       expect(component.find('Popper')).toBe(0)
+//     it('when user click span should work handleToggle', () => {
+//       expect(component.find('MenuList')).toBe(0)
 //
 //       component.find('#spanForHandleToggle').simulate('click')
 //
-//       expect(component.find('Popper')).toBe(1)
+//       expect(component.find('MenuList')).toBe(1)
+//     })
+//     it('user click away should work handleClose', () => {
+//       expect(component.find('MenuList')).toBe(1)
+//
+//       component.find('#spanForHandleToggle').simulate(CloseEvent)
+//
+//       expect(component.find('MenuList')).toBe(0)
 //     })
 //   })
 // })
