e7229092ba6ce3f2e3708364049e34e84a1946c4
OksanaMarysheva <87086991+oksanamarysheva@users.noreply.github.com>
2021-10-16 02:12:09 +0300

    Tested RegisterGooglePage component.

---
+++ src/frontend/src/components/Register/RegisterGooglePage/RegisterGooglePage.test.js
@@ -0,0 +1,44 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import RegisterGooglePage from './RegisterGooglePage'
+import * as redux from 'react-redux'
+
+jest.mock('./styles', () => () => ({}))
+
+const setUp = () => shallow(<RegisterGooglePage />)
+
+describe('should render RegisterGooglePage component', () => {
+  let spyOnUseSelector
+  let spyOnUseDispatch
+  let mockDispatch
+
+  let component
+
+  beforeEach(() => {
+    spyOnUseSelector = jest.spyOn(redux, 'useSelector')
+    spyOnUseSelector.mockReturnValue(false)
+    spyOnUseDispatch = jest.spyOn(redux, 'useDispatch')
+    mockDispatch = jest.fn()
+    spyOnUseDispatch.mockReturnValue(mockDispatch)
+
+    component = setUp()
+  })
+  it('to match snapshot', () => {
+    expect(component).toMatchSnapshot()
+  })
+  it('should work dispatch sign out', () => {
+    expect(mockDispatch).toHaveBeenCalledTimes(0)
+    component.find('Link[id=\'signOut\']').simulate('click')
+    expect(mockDispatch).toHaveBeenCalledTimes(1)
+  })
+  it('should work dispatch sign in', () => {
+    expect(mockDispatch).toHaveBeenCalledTimes(0)
+    component.find('Link[id=\'signIn\']').simulate('click')
+    expect(mockDispatch).toHaveBeenCalledTimes(1)
+  })
+  // it('should work submit handle', () => {
+  //   expect(mockDispatch).toHaveBeenCalledTimes(0)
+  //   component.find('Formik').simulate('submit')
+  //   expect(mockDispatch).toHaveBeenCalledTimes(1)
+  // })
+})
