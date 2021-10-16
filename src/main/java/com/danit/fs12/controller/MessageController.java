d7dbeb9c5c2c5b5ae66102589b7e785e3dbfc041
Serhii <romaniuk87@gmail.com>
2021-10-16 17:39:07 +0300

    -- small fixed

--- src/main/java/com/danit/fs12/controller/MessageController.java
+++ src/main/java/com/danit/fs12/controller/MessageController.java
@@ -9,13 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -65,7 +59,7 @@ public class MessageController {
   }
 
   @GetMapping(path = "/new_number")
-  public Long getNumberOfNewMessages(){
+  public Long getNumberOfNewMessages() {
     return messageFacade.getNumberOfNewMessages();
   }
 
