7c8a226c323eaec463df193d70476740ad1145c9
OksanaMarysheva <87086991+OksanaMarysheva@users.noreply.github.com>
2021-10-16 11:42:48 +0300

    Tested Login components (#210)
    * Tested Login component.
    * Added tests for ProfileMain component.
    * Tested LoginCard component.
    
    Co-authored-by: DmytroMorozTech <62470897+DmytroMorozTech@users.noreply.github.com>

--- src/frontend/src/setupTests.js
+++ src/frontend/src/setupTests.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import 'regenerator-runtime/runtime'
 import enableHooks from 'jest-react-hooks-shallow'
 import Enzyme, { shallow, render, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
