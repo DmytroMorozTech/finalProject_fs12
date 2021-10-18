74ac2e50d68142e9449259894fad949fc7cf8e78
Serhii <romaniuk87@gmail.com>
2021-10-19 00:20:22 +0300

    -- tested commentService

--- src/test/java/com/danit/fs12/service/CommentServiceUnitTest.java
+++ src/test/java/com/danit/fs12/service/CommentServiceUnitTest.java
@@ -3,14 +3,15 @@ package com.danit.fs12.service;
 import com.danit.fs12.entity.comment.Comment;
 import com.danit.fs12.entity.comment.CommentRq;
 import com.danit.fs12.entity.commentlike.CommentLike;
-import com.danit.fs12.entity.notification.Notification;
 import com.danit.fs12.entity.post.Post;
 import com.danit.fs12.entity.user.User;
-import com.danit.fs12.repository.*;
+import com.danit.fs12.repository.CommentRepository;
+import com.danit.fs12.repository.PostRepository;
+import com.danit.fs12.repository.RepositoryInterface;
+import com.danit.fs12.repository.UserRepository;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -23,7 +24,7 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class CommentServiceUnitTest {
     @Mock
     private UserRepository userRepository;
@@ -32,42 +33,56 @@ public class CommentServiceUnitTest {
     @Mock
     private UserService userService;
     @Mock
-    private CommentRepository commentRepository;
-    @Mock
     private RepositoryInterface<Comment> repo;
     @InjectMocks
     private CommentService commentService;
     @Mock
     private NotificationService notificationService;
 
-    private static final User userTest = mock(User.class);
-    private static final Comment commentTest = mock(Comment.class);
-    private static final Post postTest = mock(Post.class);
-    private static final Notification notificationTest = mock(Notification.class);
-    private static final CommentRq commentRqTest = mock(CommentRq.class);
+    private static final User userTest = new User();
+    private static final Comment commentTest = new Comment();
+    private static final Post postTest = new Post();
+    private static final CommentRq commentRqTest = new CommentRq();
+    private static final CommentLike commentLikeTest = new CommentLike();
     private static final Long userTestId = 1L;
     private static final Long postTestId = 5L;
     private static final Long commentId = 10L;
-    private static final Long notificationId = 15L;
+    private static final Long commentLikeTestId = 15L;
     private static final String postTestText = "Post1 text";
     private static final String commentTestText = "Comment1 text";
     private static final List<CommentLike> commentLikesTest = new ArrayList<>();
-    private static final List<Comment> commentsListTest= new ArrayList<>();
+    private static final List<Comment> commentsListTest = new ArrayList<>();
 
-//    @BeforeAll
-//    public void setup() {
-//        userTest.setId(userTestId);
-//        commentTest.setId(postIdFromRq);
-//        commentTest.setText(postTextFromRq);
-//        commentTest.setUser(userTest);
-//        commentTest.setCommentLikes(commentLikesTest);
-//        notificationTest.setId(notificationId);
-////        commentRqTest.setPostId(postIdFromRq);
-////        commentRqTest.setText(postTextFromRq);
-//        commentsListTest.add(commentTest);
-//        postTest.setComments(commentsListTest);
-//        postTest.setUser(userTest);
-//    }
+    @Before
+    public void setup () {
+        userTest.setId(userTestId);
+        userTest.setFirstName("UserTest FirstName");
+        userTest.setLastName("UserTest LastName");
+
+        commentTest.setId(commentId);
+        commentTest.setUser(userTest);
+
+        postTest.setId(postTestId);
+        postTest.setText(postTestText);
+        postTest.setComments(commentsListTest);
+        postTest.setUser(userTest);
+
+        commentRqTest.setPostId(postTestId);
+        commentRqTest.setText(commentTestText);
+
+        commentsListTest.add(commentTest);
+
+        commentLikeTest.setId(commentLikeTestId);
+        commentLikeTest.setUser(userTest);
+
+        commentLikesTest.add(commentLikeTest);
+        commentLikeTest.setComment(commentTest);
+        commentLikeTest.setUser(userTest);
+
+        commentTest.setCommentLikes(commentLikesTest);
+        commentTest.setPost(postTest);
+        commentTest.setText(commentTestText);
+    }
 
     @Before
     public void prepare() {
@@ -78,53 +93,35 @@ public class CommentServiceUnitTest {
 
     @Test
     public void canCreateComment() {
-        userTest.setId(userTestId);
-        userTest.setFirstName("UserTest FirstName");
-        userTest.setLastName("UserTest LastName");
-
-        commentTest.setId(commentId);
-
-        postTest.setText(postTestText);
-        postTest.setComments(commentsListTest);
-        postTest.setUser(userTest);
-        System.out.println(postTest.getId());
-
-        commentRqTest.setPostId(postTestId);
-        commentRqTest.setText(commentTestText);
-
         Long postId = commentRqTest.getPostId();
         String text = commentRqTest.getText();
 
-        List<CommentLike> commentLikes = new ArrayList<>();
-
         when(userService.getActiveUser()).thenReturn(userTest);
-        when(userService.getActiveUser().getId()).thenReturn(userTestId);
         when(userRepository.findEntityById(userTestId)).thenReturn(userTest);
         when(postRepository.findEntityById(postId)).thenReturn(postTest);
 
         postTest.getComments().add(commentTest);
         userTest.getComments().add(commentTest);
 
-        when(repo.save(new Comment(text, postTest, userTest, commentLikes))).thenReturn(commentTest);
+        when(repo.save(new Comment(text, postTest, userTest, commentLikesTest))).thenReturn(commentTest);
         when(userRepository.save(userTest)).thenReturn(userTest);
         when(postRepository.save(postTest)).thenReturn(postTest);
 
-//        User newUser = new User();
-//        newUser.setFirstName("Unit1");
-//        newUser.setLastName("Test1");
-//        newUser.setId(2L);
-//        Post newPost = new Post();
-//        newPost.setId(18L);
-//        newPost.setText(postTextFromRq);
-//        Long newPostId = newPost.getId();
-//        newPost.setUser(newUser);
-//        System.out.println(newPost.getUser());
         User postAuthor = postTest.getUser();
         Long postAuthorId = postAuthor.getId();
 
         doNothing().when(notificationService).createNotificationComment(postTestId, postAuthorId);
-//        notificationService.createNotificationComment(postTest.getId(), postTest.getUser().getId());
 
         Assert.assertEquals(commentTest.getText(), commentService.createComment(commentRqTest).getText());
+    }
+
+    @Test
+    public void canToggleCommentLike() {
+        when(userService.getActiveUser()).thenReturn(userTest);
+        when(repo.findEntityById(commentId)).thenReturn(commentTest);
+        when(userRepository.findEntityById(userTestId)).thenReturn(userTest);
+        when(repo.save(commentTest)).thenReturn(commentTest);
+
+        Assert.assertEquals(commentTestText, commentService.toggleCommentLike(commentId).getText());
     }
 }
