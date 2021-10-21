36ce1834ccc45ef545c87fcf1655d39383837258
DmytroMorozTech <62470897+DmytroMorozTech@users.noreply.github.com>
2021-10-21 16:06:27 +0300

    -set correct timeZone (Europe/Kiev) (#238)

--- src/main/java/com/danit/fs12/Application.java
+++ src/main/java/com/danit/fs12/Application.java
@@ -3,8 +3,16 @@ package com.danit.fs12;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kiev"));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
