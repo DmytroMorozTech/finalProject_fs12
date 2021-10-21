d28487374699e696d73c0b7368f6d8711bcb2c95
DmytroMorozTech <62470897+DmytroMorozTech@users.noreply.github.com>
2021-10-21 11:21:45 +0300

    - added possibility to copy link to a post without using HTTPS connection (#237)

--- src/frontend/src/components/Main/Feed/Post/PostButton/PostButtons.js
+++ src/frontend/src/components/Main/Feed/Post/PostButton/PostButtons.js
@@ -44,9 +44,30 @@ function PostButtons (props) {
   const handleLike = singlePostRender ? handleLikeSingle : handleLikeGeneral
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(linkToPost)
+    copyToClipboard(linkToPost)
     handleTooltipOpen()
     setTimeout(() => handleTooltipClose(), 600)
+  }
+
+  function copyToClipboard (text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+      // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+      return window.clipboardData.setData('Text', text)
+    } else if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+      const textarea = document.createElement('textarea')
+      textarea.textContent = text
+      textarea.style.position = 'fixed' // Prevent scrolling to bottom of page in Microsoft Edge.
+      document.body.appendChild(textarea)
+      textarea.select()
+      try {
+        return document.execCommand('copy') // Security exception may be thrown by some browsers.
+      } catch (ex) {
+        console.warn('Copy to clipboard failed.', ex)
+        return false
+      } finally {
+        document.body.removeChild(textarea)
+      }
+    }
   }
 
   const handleSend = () => {
